af882a01d8db93b169e6ef1460d9025477df52e7
MEHDIBLABO <Mehdiblabo@gmail.com>
2021-03-11 20:42:38 +0100

    client_mad_compte

---
+++ ATELIER1_cshap_client_mad_compte/client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLCLIENT_COMPTE
+{
+    class client
+    {
+        public client(string n, string p, string a)
+        {
+            this.nom = n;
+            this.prenom = p;
+            this.adresse = a;
+
+        }
+
+        public void afficher()
+        {
+            Console.WriteLine("le nom est " + this.nom + "le prenom est :" + this.prenom + "l adresse est :" + this.adresse);
+        }
+
+
+
+        private string nom, prenom, adresse;
+    }
+
+}
